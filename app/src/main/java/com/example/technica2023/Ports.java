75a3de89b28b02492b0684e70d81d38dcfe7e3a2
Michelle Lu <mlu7@terpmail.umd.edu>
2023-10-22 11:59:39 -0400

    commenting out port scan

--- app/src/main/java/com/example/technica2023/Ports.java
+++ app/src/main/java/com/example/technica2023/Ports.java
@@ -23,16 +23,18 @@ public class Ports extends AppCompatActivity {
     public void scan(View v) throws UnknownHostException {
         String open_ports = "test";
         InetAddress localhost = InetAddress.getByName("127.0.0.1");
-        int common_ports[] = {7,20,21,22,23,25,53,69,80,88,135,139,443,464,465,587,636,3724};
-        for(int i : common_ports){
+        //int common_ports[] = {7,20,21,22,23,25,53,69,80,88,135,139,443,464,465,587,636,3724,8080};
+        /*for(int i = 1; i < 9999; i++){
             try{
                 Socket socket = new Socket(localhost,i);
                 open_ports += "" + i + ", ";
+                open_ports += "ljdslkfj";
                 socket.close();
             } catch (IOException e) {
                 // port is not open!
+
             }
-        }
+        }*/
         TextView tv = findViewById(R.id.ports_tv);
         tv.setText(open_ports);
     }
