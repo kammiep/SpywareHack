aab9bbd9d3fa36a74e76f0d623c034f1fae8314b
kammiep <kping@terpmail.umd.edu>
2023-10-22 01:12:49 -0400

    added some about page things and removed unused imports

--- app/src/main/java/com/example/technica2023/AboutPage.java
+++ app/src/main/java/com/example/technica2023/AboutPage.java
@@ -1,5 +1,15 @@
 package com.example.technica2023;
 
-public class AboutPage {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_page);
+    }
+
 
 }
